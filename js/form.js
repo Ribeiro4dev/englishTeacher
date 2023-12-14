238d872ff6aa226d5e8be9e294d1c6b55f494e0f
Bruno G. Ribeiro <brunoribeirosst@gmail.com>
2023-12-13 22:00:21 -0300

    commitando modificações no js e css

--- js/form.js
+++ js/form.js
@@ -11,22 +11,22 @@ inputNome.addEventListener("focus", () => {
 
 inputNome.addEventListener("blur", (e) => {    
     if (e.target.value === "") {
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#f0627f";
         alert("O campo não pode estar vazio.");
     }else if(!e.target.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').match(/^[a-zA-Z ]+$/)){
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#f0627f";
         alert("O campo deve conter apenas letras.");
 
-    }else if (e.target.value.length < 5) {
-        e.target.style.backgroundColor = "#FF0000";
-        alert("O campo deve conter no mínimo 5 caracteres.");
+    }else if (e.target.value.length < 3) {
+        e.target.style.backgroundColor = "#f0627f";
+        alert("O campo deve conter no mínimo 3 caracteres.");
     } else if(e.target.value.length > 50) {
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#f0627f";
         alert("O campo deve conter no máximo 50 caracteres.");
         
     }else {
         
-        e.target.style.backgroundColor = "#00FF00";
+        e.target.style.backgroundColor = "#5ac45a";
     }
 });
 
@@ -38,13 +38,13 @@ inputEmail.addEventListener("blur", (e) => {
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const valor = e.target.value.trim();
     if (valor === "") {
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#f0627f";
         alert("O campo não pode estar vazio.");
     } else if (!valor.match(regexEmail)) {
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#f0627f";
         alert("Por favor, digite um e-mail válido.");
     }else{
-        e.target.style.backgroundColor = "#00FF00";
+        e.target.style.backgroundColor = "#5ac45a";
     }
 });
 
@@ -55,12 +55,12 @@ inputTelefone.addEventListener("focus", () => {
 inputTelefone.addEventListener("blur", (e) => {      
     const valor = inputTelefone.value.trim();
     if (!valor.match(/^[0-9]+$/)) {        
-        e.target.style.backgroundColor = "#FF0000";
+        e.target.style.backgroundColor = "#f0627f";
         e.target.value = ""
-        alert("O campo deve conter apenas números."); 
+        alert("O campo não pode estar vazio e deve conter apenas números."); 
     }else {
         inputTelefone.setCustomValidity("");
-        e.target.style.backgroundColor = "#00FF00";
+        e.target.style.backgroundColor = "#5ac45a";
     }
     
 });
@@ -72,10 +72,10 @@ selectPlano.addEventListener("focus", () => {
 selectPlano.addEventListener("blur", (e) => {    
     const valor = document.querySelector("select[name='planos']").value;    
     if (valor === "") {
-        e.target.style.backgroundColor = "#FF0000";        
+        e.target.style.backgroundColor = "#f0627f";        
         alert("Por favor, selecione um plano.");        
     }else {
-        e.target.style.backgroundColor = "#00FF00";
+        e.target.style.backgroundColor = "#5ac45a";
     }
 });
 
@@ -87,16 +87,21 @@ inputMensagem.addEventListener("blur", (e) => {
     e.target.style.backgroundColor = "";
 });
 
-
 btnSubmit.addEventListener("click", (e) => {    
-    if (inputMensagem.value === "") {        
-        alert("Por favor, preencha todos os campos.");
+    if (!(inputNome.value.trim() 
+    && inputEmail.value.trim() 
+    && inputTelefone.value.trim() 
+    && selectPlano.value.trim() 
+    && inputMensagem.value.trim() !== "")) {
+        e.preventDefault();
+        alert("Por favor, corrija os erros no formulário antes de enviar.");
     } else {
-        e.preventDefault();         
-        alert("formulário enviado com sucesso!");
-        window.location.href = 'index.html';
+        alert("Formulário enviado com sucesso!");
+        window.location.href = 'dicas.html';
     }
-})
+});
+
+
 
 
 
